65f114e796dea173b5e3e02fd0d435af6e27c03f
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2022-10-02 18:53:26 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,12 +1,14 @@
 def solution(nums):
-    N, tmp, result = len(nums), set(), 0
+    N, tmp, result = len(nums), [], 0
     for i1 in range(N-2):
         for i2 in range(i1+1, N-1):
             for i3 in range(i2+1, N):
-                num = (nums[i1] + nums[i2] + nums[i3])
-                for i in range(2, num):
-                    if num%i == 0:
-                        break
-                else:
-                    result += 1
+                tmp.append(nums[i1] + nums[i2] + nums[i3])
+
+    for num in tmp:
+        for i in range(2, num):
+            if num%i == 0:
+                break
+        else:
+            result += 1
     return result
