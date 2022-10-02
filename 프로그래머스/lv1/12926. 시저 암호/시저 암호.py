d6ccb36b9c2c9629669f39fc99fc6c72623b079d
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2022-10-02 19:48:23 +0900

    [level 1] Title: 시저 암호, Time: 1.59 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,17 @@
+def solution(s, n):
+    answer = ''
+    for i in s:
+        if i == ' ':
+            answer += i
+            continue
+        if ord(i) < 97:
+            if ord(i) + n > 90:
+                answer += chr(ord(i) +n - 26)
+            else:
+                answer += chr(ord(i) +n)
+        else:
+            if ord(i) + n > 122:
+                answer += chr(ord(i) +n - 26)
+            else:
+                answer += chr(ord(i) +n)
+    return answer
