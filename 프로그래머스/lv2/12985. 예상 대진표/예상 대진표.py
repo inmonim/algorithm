c632fb41efed271a2616d695956e917ddb2b9937
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2022-10-05 23:47:05 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,7 +1,2 @@
 def solution(n,a,b):
-    cnt = 0
-    while a != b:
-        a = a//2 if a%2 == 0 else a//2 + 1
-        b = b//2 if b%2 == 0 else b//2 + 1
-        cnt += 1
-    return cnt
+    return ((a-1)^(b-1)).bit_length()
