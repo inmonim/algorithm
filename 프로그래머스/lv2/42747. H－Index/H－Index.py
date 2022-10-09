220d66bd6a2b482d1ba068e4719a027f7793d08f
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2022-10-09 17:55:51 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,9 +1,9 @@
 def solution(citations):
-    citations.sort()
+    citations.sort(reverse=True)
     L = len(citations)
-    for i in range(len(citations), 0, -1):
-        if citations[i-1] <= L - i:
+    for i in range(L):
+        if citations[i] < i+1:
             break
     else:
         return L
-    return L-i
+    return i
