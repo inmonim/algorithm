ae7c6b2aa206a267a8adf5a0f353e10569a86334
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2022-10-17 13:05:00 +0900

    [Silver IV] Title: 수열 정렬, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1015. 수열 정렬/수열 정렬.py
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.py
@@ -1,17 +1,5 @@
-N = int(input())
-arr = list(map(int, input().split()))
-
-result = ['0'] * N
-
-cnt = 0
-while sum(arr) != 1001*N:
-    M = min(arr)
-    for i in range(len(arr)):
-        if i == len(arr):
-            break
-        elif arr[i] == M:
-            arr[i] = 1001
-            result[i] = str(cnt)
-            cnt += 1
-
-print(' '.join(result))
+input()
+A=list(map(int,input().split()))
+R=sorted(A)
+for i in range(len(A)):A[A.index(R[i])]=str(i)
+print(' '.join(A))
