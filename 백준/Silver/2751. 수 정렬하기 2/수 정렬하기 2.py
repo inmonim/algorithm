9bab11c3fccbc4e51ba0baa03b79af50a552b5cb
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2022-11-09 10:28:51 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1432 ms, Memory: 86020 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,11 +1,4 @@
 import sys
-N = int(input())
-arr = [int(sys.stdin.readline().strip()) for _ in range(N)]
-dic = {i:0 for i in range(min(arr),max(arr)+1)}
-for i in arr:
-    dic[i] = 1
-R = []
-for k,v in dic.items():
-    if v == 1:
-        R.append(k)
-print(*R, sep='\n')
+N = int(sys.stdin.readline())
+arr = sorted([int(sys.stdin.readline().strip()) for _ in range(N)])
+print(*arr, sep='\n')
