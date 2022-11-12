5a85687de672a17bf2327084c18560f64eb9f923
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2022-11-13 01:54:46 +0900

    [Silver II] Title: 나무 자르기, Time: 608 ms, Memory: 148396 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,27 @@
+import sys
+
+n,m = map(int, sys.stdin.readline().split())
+
+arr = list(map(int, sys.stdin.readline().split()))
+dic = {}
+
+for i in arr:
+    if i not in dic:
+        dic[i] = 1
+    else:
+        dic[i] += 1
+        
+T = sorted(dic.keys(), reverse=True)
+R = 0
+mul = 0
+for i in range(1, len(T)+1):
+    if i < len(T):
+        mul += dic[T[i-1]]
+        R += (T[i-1] - T[i]) * mul
+        if R >= m:
+            print(T[i] + (R-m)//mul)
+            break
+    else:
+        mul += dic[T[i-1]]
+        R += (T[i-1]) * mul
+        print((R-m)//mul)
