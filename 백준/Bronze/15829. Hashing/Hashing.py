29bc7965dd569da9a3f7c25d6dcde10249029c57
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2022-11-14 02:57:58 +0900

    [Bronze II] Title: Hashing, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,11 +1,12 @@
-L=int(input())
+input()
 R = 0
 r = 1
-txt = input()
-for i in range(L):
-    if i == 0:
-        R += ord(txt[i])-96
+cnt = 0
+for i in input():
+    if cnt == 0:
+        R += ord(i)-96
+        cnt+=1
     else:
-        R += (ord(txt[i])-96)*((r:=r*31))
+        R += (ord(i)-96)*(r:=r*31)
 
 print(R%1234567891)
