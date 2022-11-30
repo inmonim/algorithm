2993b9b771043310caa9a2f1be4c17a29d6bfb3a
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2022-11-30 15:39:49 +0900

    [Silver II] Title: 유기농 배추, Time: 336 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,25 @@
+for T in range(int(input())):
+    M,N,K = map(int, input().split())
+    A = 0
+    arr = [[0]*M for _ in range(N)]
+    for i in range(K):
+        x,y = map(int, input().split())
+        arr[y][x] = 1
+        
+    for y in range(N):
+        for x in range(M):
+            stack = []
+            if arr[y][x] == 1:
+                stack = [(y,x)]
+                A += 1
+            while stack:
+                sy, sx = stack.pop(0)
+                arr[y][x] = 0
+                for dy,dx in [(-1,0),(0,1),(1,0),(0,-1)]:
+                    ny, nx = dy+sy, dx+sx
+                    if 0 <= ny < N and 0<= nx < M:
+                        if arr[ny][nx] == 1:
+                            arr[ny][nx] = 0
+                            stack.append((ny,nx))
+                            
+    print(A)
