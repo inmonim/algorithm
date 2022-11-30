683bdf5071f844c2660a74b294498b99183c52a9
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2022-11-30 14:10:08 +0900

    [Silver II] Title: 마인크래프트, Time: 236 ms, Memory: 32296 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,57 +1,37 @@
 N,M,B = map(int, input().split())
-
-answer = [0,0]
-
+A = [0,0]
 Map = []
 for i in range(N):
     Map.append(list(map(int, input().split())))
-
-dic = {}
-
+D = {}
 for i in Map:
     for j in i:
-        if dic.get(int(j)):
-            dic[int(j)] += 1
+        if D.get(int(j)):
+            D[int(j)] += 1
         else:
-            dic[int(j)] = 1
-
-bottom = min(dic.keys())
-bottom_val = dic[bottom]
-
-top = max(dic.keys())
-top_val = dic[top] * 2
-
-answer[1] = top
-
-while len(dic) != 1:
-    bottom = min(dic.keys())
-    bottom_val = dic[bottom]
-
-    top = max(dic.keys())
-    top_val = dic[top] * 2
-
-    answer[1] = top
-
-    if bottom_val <= top_val and dic[bottom] <= B:
-        answer[0] += bottom_val
-        if dic.get(bottom+1):
-            dic[bottom+1] += dic[bottom]
+            D[int(j)] = 1
+t = max(D.keys())
+A[1] = t
+while len(D) != 1:
+    b = min(D.keys())
+    b_v = D[b]
+    t = max(D.keys())
+    t_v = D[t] * 2
+    if b_v <= t_v and D[b] <= B:
+        A[0] += b_v
+        if D.get(b+1):
+            D[b+1] += D[b]
         else:
-            dic[bottom+1] = dic[bottom]
-        B -= dic[bottom]
-        del dic[bottom]
-        
+            D[b+1] = D[b]
+        B -= D[b]
+        del D[b]
     else:
-        answer[0] += top_val
-        if dic.get(top-1):
-            dic[top-1] += dic[top]
+        A[0] += t_v
+        if D.get(t-1):
+            D[t-1] += D[t]
         else:
-            dic[top-1] = dic[top]
-        B += dic[top]
-        del dic[top]
-        answer[1] = max(dic.keys())
-
-    if len(dic) == 1:
-        break
-
-print(*answer)
+            D[t-1] = D[t]
+        B += D[t]
+        del D[t]
+        A[1] = max(D.keys())
+print(*A)
