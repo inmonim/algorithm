4584ce0645739582bfd4dc615640fc23be02561b
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2022-12-15 12:51:04 +0900

    [Silver III] Title: 1로 만들기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,31 @@
+import sys
+
+Q = [int(sys.stdin.readline().strip())]
+
+def sol(n):
+    if check.get(n) == None:
+        next_li.append(n)
+        check[n] = 1
+
+check= {}
+TF = 1
+cnt = 0
+next_li = []
+while TF:
+    Q.extend(next_li)
+    next_li = []
+    cnt += 1
+    while Q:
+        n = Q.pop(0)
+        if n%3 == 0:
+            sol(n//3)
+        if n%2 == 0:
+            sol(n//2)
+        sol(n-1)
+        if n == 1 or 1 in next_li:
+            if n == 1:
+                print(0)
+            else:
+                print(cnt)
+            TF = 0
+            break
