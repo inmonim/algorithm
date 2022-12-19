532aa0c128506dcf771ca3bbabbcb7743a7548bb
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2022-12-19 13:17:30 +0900

    [Silver I] Title: 숨바꼭질, Time: 5480 ms, Memory: 42216 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def check(n):
     if n < 0 or n > 100000:
         return
@@ -9,23 +11,22 @@ N,M = map(int, input().split())
 
 vis = {}
 cnt = 0
-Q = [N]
-next_li = []
+next_li = [N]
 
-while Q:
-    n = Q.pop(0)
-    check(n*2)
-    check(n+1)
-    check(n-1)
-    if n == M or M in next_li:
-        if n == M:
-            print(0)
-        else:
-            cnt += 1
-            print(cnt)
-        break
-    if not Q:
-        cnt += 1
-        Q.extend(next_li)
-        next_li = []
-        
+
+while next_li:
+    Q = deque(next_li)
+    next_li = []
+    cnt += 1
+    while Q:
+        n = Q.popleft()
+        check(n*2)
+        check(n+1)
+        check(n-1)
+        if n == M or M in next_li:
+            if n == M:
+                print(0)
+            else:
+                print(cnt)
+            next_li = []
+            break
