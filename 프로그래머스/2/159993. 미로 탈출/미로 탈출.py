fcccfb98996836ad0e19cbf037e21d910c654cce
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2023-11-09 15:38:03 +0900

    [level 2] Title: 미로 탈출, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -0,0 +1,38 @@
+def solution(maps):
+    max_y, max_x = len(maps), len(maps[0])
+    for y in range(len(maps)):
+        for x in range(len(maps[0])):
+            if maps[y][x] == "S":
+                start = [y, x]
+    
+    visited = [[0] * len(maps[0]) for _ in range(len(maps))]
+    cnt = 0
+    
+    def bfs(maps, next_step, target, visit, cnt):
+        next_step = [next_step]
+        while next_step:
+            starts = next_step
+            next_step = []
+            for yx in starts:
+                y, x = yx
+                for dy, dx in ((0, 1), (1, 0), (-1, 0), (0,-1)):
+                    ny, nx = y+dy, x+dx
+                    if (ny >= 0 and ny < max_y and nx >= 0 and nx < max_x and maps[ny][nx] != "X"):
+                        
+                        if visit[ny][nx] != '-':
+                            next_step.append([ny,nx])
+                            visit[ny][nx] = '-'
+
+                        if maps[ny][nx] == target and maps[ny][nx] == 'L':
+                            visit = [[0] * len(maps[0]) for _ in range(len(maps))]
+                            return bfs(maps, [ny, nx], 'E', visit, cnt + 1)
+                        
+                        elif maps[ny][nx] == target and maps[ny][nx] == 'E':
+                            return cnt + 1
+            cnt += 1
+        else:
+            return -1
+
+    answer = bfs(maps, start, 'L', visited, cnt)
+
+    return answer
