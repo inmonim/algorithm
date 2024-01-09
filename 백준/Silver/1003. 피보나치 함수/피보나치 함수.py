0aff718a545c91cdc4569d1cad8dd4fb239dc680
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-01-09 18:02:39 +0900

    [Silver III] Title: 피보나치 함수, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,13 +1,8 @@
-for _ in range(int(input())):
-    N = int(input())
-    Z = [1,0]
-    O = [0,1]
-    for i in range(1, N):
-        Z.append(Z[-2] + Z[-1])
-        O.append(O[-2] + O[-1])
-    if N == 0:
-        print('1 0')
-    elif N == 1:
-        print('0 1')
-    else:
-        print(f'{Z[-1]} {O[-1]}')
+t=input
+for _ in range(int(t())):
+    x=int(t())
+    Z,O=[1,0,1],[0,1,1]
+    for i in range(3, x+1):
+        Z.append(Z[i-2]+Z[i-1])
+        O.append(O[i-2]+O[i-1])
+    print(f'{Z[x]} {O[x]}')
