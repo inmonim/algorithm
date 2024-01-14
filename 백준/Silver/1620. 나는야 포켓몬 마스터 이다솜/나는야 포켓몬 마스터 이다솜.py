188f5a23fd35824dcc3662f6ea9cb747a4a9027d
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-01-14 18:03:59 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 212 ms, Memory: 47696 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -4,16 +4,14 @@ input = sys.stdin.readline
 
 N,M = map(int, input().split())
 
-idx = {}
-name = {}
-
+n_d = {}
 for i in range(N):
-    idx[1+i] = input().strip()
-    name[idx[1+i]] = 1+i
-    
+    n_d[input().strip()] = i+1
+n_l = [0]+list(n_d.keys())
+
 for i in range(M):
-    x = input().strip()
-    if x.isdigit():
-        print(idx[int(x)])
+    a = input().strip()
+    if a.isdigit():
+        print(n_l[int(a)])
     else:
-        print(name[x])
+        print(n_d[a])
