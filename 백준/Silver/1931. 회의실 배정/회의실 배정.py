83adc0a795ce59ea97129685305066b2564f0495
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-01-15 18:32:49 +0900

    [Silver I] Title: 회의실 배정, Time: 336 ms, Memory: 60092 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+l = []
+
+for i in range(int(input())):
+    l.append(list(map(int, input().split())))
+
+l.sort(key=lambda x:[x[1], x[0]])
+
+ep = 0
+a = 0
+
+for s, e in l:
+    if ep <= s:
+        a += 1
+        ep = e
+
+print(a)
