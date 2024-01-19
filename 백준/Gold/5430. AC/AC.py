b6f330638587a0b3ff7cc99c48a3703a6ff12bbf
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-01-19 13:32:48 +0900

    [Gold V] Title: AC, Time: 3716 ms, Memory: 38476 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -6,14 +6,15 @@ for t in range(int(input())):
     func = input()
     l = int(input())
     arr = []
+    arr_string = input().strip()
     string = ''
-    for s in input().strip()[1:]:
-        if s.isdigit():
-            string += s
-        else:
-            if string != '':
+    if l:
+        for s in arr_string[1:]:
+            if s.isdigit():
+                string += s
+            else:
                 arr.append(string)
-            string = ''
+                string = ''
     
     p = 0
     for f in func:
@@ -29,5 +30,5 @@ for t in range(int(input())):
             del arr[p]
     else:
         if p == -1:
-            arr = arr[::-1]
+            arr.reverse()
         print(f"[{','.join(arr)}]")
