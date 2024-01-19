d21ba7cab58c6eb392759231fde63a12b81ad83c
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-01-19 15:32:35 +0900

    [Silver I] Title: IOIOI, Time: 2380 ms, Memory: 43856 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -14,13 +14,21 @@ dis = re.sub('I{3,}', 'II', dos)
 
 ans = 0
 l = len(ioi)
+flag = 0
 
 i = 0
 while i < len(dis):
-    if dis[i] == 'I':
-        if dis[i:i+l] == ioi:
+    if flag:
+        if dis[i:i+2] == 'OI':
             ans += 1
             i += 1
+        else:
+            flag = 0
+    if not flag and dis[i] == 'I':
+        if dis[i:i+l] == ioi:
+            ans += 1
+            i += l-1
+            flag = 1
     i += 1
 
 print(ans)
