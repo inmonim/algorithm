86a085f3a656c28c752d32d945115a63af3ec999
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-01-30 15:23:00 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -2,4 +2,7 @@ import sys
 input = sys.stdin.readline
 
 N, arr = int(input()), sorted(list(map(int, input().split())))
-print(sum([sum(arr[0:i+1]) for i in range(N)]))
+a = 0
+for i in range(N):
+    a += sum(arr[0:i+1])
+print(a)
