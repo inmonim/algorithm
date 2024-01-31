50f13a2e2e6f038b33e9d36aa22a9d9f687ffc1d
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-01-31 21:42:23 +0900

    [Silver V] Title: 집합, Time: 3932 ms, Memory: 36188 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,14 +1,18 @@
+from array import array
 import sys
 
 input = sys.stdin.readline
 
-S = [0]*21
+from array import array
+
+S = array('i', [0]*21)
 S1 = [i for i in range(21)]
+
 for _ in range(int(input())):
     op = input().split()
     if len(op) == 1:
         if op[0][0] == 'a':
-            S = S1.copy()
+            S = array('i', S1)
         else:
             S = [0]*21
         continue
@@ -18,10 +22,10 @@ for _ in range(int(input())):
         S[n] = 1
     elif o[0] == 'r':
         S[n] = 0
-    elif o[0] == 'c':
+    elif o[0] == 't':
+        S[n] = 0 if S[n] else 1
+    else:
         if S[n]:
             print(1)
         else:
             print(0)
-    elif o[0] == 't':
-        S[n] = 0 if S[n] else 1
