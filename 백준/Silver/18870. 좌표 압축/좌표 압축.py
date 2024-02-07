abf0cd867040e471d53497cacfd384a89ef08bb0
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-02-07 23:36:25 +0900

    [Silver II] Title: 좌표 압축, Time: 1592 ms, Memory: 204124 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,7 +1,7 @@
 N = int(input())
 arr = list(map(int, input().split()))
 s_l = sorted(list({i:0 for i in arr}.keys()))
-i_d = {s_l[i]:i for i in range(len(s_l))}
-ans = [i_d[i] for i in arr]
+i_d = {v:i for i, v in enumerate(s_l)}
+ans = [str(i_d[i]) for i in arr]
 
-print(' '.join(map(str, ans)))
+print(' '.join(ans))
