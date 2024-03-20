5807e69aeffbc0e62b0769d6aea4b76c4a3ebcd9
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-03-20 21:53:58 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,12 +1,1 @@
-a, b, c = map(int, input().split())
-
-def dac(a, b, c):
-
-    if b == 1:
-        return a % c
-    elif not b % 2:
-        return (dac(a, b//2, c) ** 2) % c
-    else:
-        return ((dac(a, b//2, c) ** 2) * a) % c
-
-print(dac(a,b,c))
+print(pow(*map(int,input().split())))
