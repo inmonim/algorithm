68998c17aab9849d7034c577582f26704623fe17
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-03-25 23:41:17 +0900

    [Gold IV] Title: 최단경로, Time: 1000 ms, Memory: 61584 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,5 +1,6 @@
 import heapq
 import sys
+
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
@@ -8,25 +9,22 @@ reach = [M*20 for _ in range(N+1)]
 reach[start] = 0
 
 node = [{} for _ in range(N+1)]
-node_list = [[] for _ in range(N+1)]
 for _ in range(M):
     s, e, v = map(int, input().split())
-    
     if e in node[s]:
         if node[s][e] > v:
             node[s][e] = v
     else:
         node[s][e] = v
-        node_list[s].append(e)
 
 Q = []
-for k in node_list[start]:
+for k in node[start].keys():
     Q.append((node[start][k], k))
     reach[k] = node[start][k]
 
 while Q:
     v, k = Q.pop(0)
-    for nk in node_list[k]:
+    for nk in node[k].keys():
         nv = v + node[k][nk]
         if reach[nk] > nv:
             reach[nk] = nv
