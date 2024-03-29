ac073eeb0101cf6862ace9d3270609d3577cdf5f
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-03-29 20:26:37 +0900

    [Silver I] Title: 정수 삼각형, Time: 112 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,30 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-tri = []
-for i in range(n := int(input())):
-    tri.append(list(map(int, input().split())))
+n = int(input())
+res = [int(input())]+[0]*n
 
-# 삼각형의 밑부분부터 순회
-for i in range(n-1, -1, -1):
-    edge = tri[i]
-    # 깊은 복사
-    up_edge = tri[i-1][:]
+for i in range(1, n):
+    tmp = res[:]
+    num = list(map(int, input().split()))
     
-    # 해당 단계의 값 순회
-    for i2 in range(i+1):
-        x = edge[i2]
-        # 윗 단계의 값 순회
-        for j in range(i2-1, i2+1):
-            if j == -1 or j == i:
-                continue
-            
-            # 깊은 복사가 이루어지지 않으면 up_edge가
-            # tri의 바뀐 값을 실시간으로 따라감
-            y = up_edge[j]
-            s = x + y
-            
-            if tri[i-1][j] < s:
-                tri[i-1][j] = s
+    res[0] = tmp[0] + num[0]
+    res[i] = tmp[i-1] + num[i]
+    
+    for j in range(1, i):
+        res[j] = max(tmp[j-1], tmp[j]) + num[j]
 
-print(tri[0][0])
+print(max(res))
