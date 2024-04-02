107b4d0f6517c611316fe92c63dd2373d39ee8c0
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-04-02 23:14:01 +0900

    [Gold V] Title: 내려가기, Time: 296 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -1,24 +1,18 @@
-import array, sys
+import sys
 
 input = sys.stdin.readline
 
 n = int(input())
-arr = array.array('i')
-for _ in range(n):
-    arr.extend(list(map(int, input().split())))
-arr2 = arr.__copy__()
 
-def find_m(route, func, mv):
-    i = 1
-    while i < n:
-        for j in range(3):
-            m = mv
-            for k in range(-1, 2):
-                if 0 <= j + k < 3:
-                    m = func(m, route[(i-1)*3+(j+k)] + route[i*3+j])
-            route[(i*3)+j] = m
-        i += 1
-    print(func(route[-3:]), end=' ')
-
-find_m(arr, max, 0)
-find_m(arr2, min, n*10)
+n_l = list(map(int, input().split()))
+dp_min = n_l
+dp_max = n_l
+for _ in range(n-1):
+    n_l = list(map(int, input().split()))
+    dp_min = [min(dp_min[0], dp_min[1]) + n_l[0],
+                min(dp_min) + n_l[1],
+                min(dp_min[1], dp_min[2]) + n_l[2]]
+    dp_max = [max(dp_max[0], dp_max[1]) + n_l[0],
+                max(dp_max) + n_l[1],
+                max(dp_max[1], dp_max[2]) + n_l[2]]
+print(max(dp_max), min(dp_min))
