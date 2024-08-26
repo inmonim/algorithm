43bcd39d602e40738a4091c55e2970aab4dcf522
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-08-26 15:05:05 +0900

    [level 2] Title: [3차] 압축, Time: 1.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -7,15 +7,19 @@ def solution(msg):
     while i < len(msg):
         tmp = 0
         for j in range(i+1, len(msg)+1):
+            # 있으면 last_idx에 대입, 임시 시작 인덱스 값 +1
             if alphabet_dict.get(msg[i:j]):
                 last_idx = alphabet_dict[msg[i:j]]
                 tmp += 1
+            # 없으면 last_idx를 answer에 삽입, dictionary_index 값 1 상승
+            # 시작 인덱스 값 +
             else:
                 alphabet_dict[msg[i:j]] = d_i
                 d_i += 1
                 answer.append(last_idx)
                 i += tmp
                 break
+        # 루프가 break없이 종료되었다는 것은 마지막 글자임을 의미, last_idx 삽입하고 알고리즘 종료
         else:
             answer.append(last_idx)
             break
