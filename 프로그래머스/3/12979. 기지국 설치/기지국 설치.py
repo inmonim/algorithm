513d991345c77c7b8b68f0058abb92ba5162b984
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-12-04 20:45:30 +0900

    [level 3] Title: 기지국 설치, Time: 4.29 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,24 +1,33 @@
 def solution(n, stations, w):
     answer = 0
-    
     nn = []
     
+    # 시작점부터 첫 기지국까지 빈 공간이 있는지
     if 1 < (stations[0] - w):
-        nn.append((1, stations[0] - w-1))
+        nn.append((1, stations[0]-w-1))
     
+    # 첫 기지국 부터 마지막 기지국 사이의 빈공간 찾기
     for i in range(len(stations)-1):
-        
         a, b = stations[i], stations[i+1]
         
-        if a + w < b - w:
+        if a + w + 1 < b - w:
             nn.append((a+w+1, b-w-1))
-            
+    
+    # 마지막 기지국 부터 끝까지 빈공간 있는지
     if stations[-1] + w < n:
         nn.append((stations[-1]+w+1, n))
     
+    # 전파 도달 거리
+    length = w*2 + 1
+    
+    # 조각난 빈공간들을 순회
     for x in nn:
-        print(x)
-        answer += (((x[1] - x[0]) + 1) // (w*2+1))
-        answer += 1 if (((x[1] - x[0]) + 1) % (w*2+1)) else 0
+
+        lose_range = (x[1] - x[0]) + 1
+        
+        # 최소한의 기지국 수
+        answer += lose_range // length
+        # 닿지 않는 남은 거리가 있는 경우 +1
+        answer += 1 if lose_range % length else 0
     
     return answer
