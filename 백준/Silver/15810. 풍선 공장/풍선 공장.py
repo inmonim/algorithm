f001a114266645f681e897a079356af1cd324182
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-12-06 04:04:46 +0900

    [Silver II] Title: 풍선 공장, Time: 888 ms, Memory: 150552 KB -BaekjoonHub

--- 백준/Silver/15810. 풍선 공장/풍선 공장.py
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.py
@@ -1,16 +1,25 @@
 import sys
-from array import array
+
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
 
 arr = list(map(int, input().split()))
-arr = array('I', arr)
 
-mm = m * min(arr)
+time_dict = {}
+
+for i in arr:
+    if i not in time_dict:
+        time_dict[i] = 1
+    else:
+        time_dict[i] += 1
+
+mm = m * min(time_dict.keys())
 t, b = mm, 1
 
 while b <= t:
-    c = (b+t)//2
-    cur = sum(c//i for i in arr)
+    c = (b + t)//2
+    cur = sum([(c//k)*v for k,v in time_dict.items()])
     
     if cur >= m:
         t = c - 1
