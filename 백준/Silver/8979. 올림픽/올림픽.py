2afedae810631c8b8c0fd844086cf17f53b10b2c
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-12-12 00:01:39 +0900

    [Silver V] Title: 올림픽, Time: 60 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -9,7 +9,7 @@ for i in range(1, len(arr)):
     if arr[i-1][0] == m:
         print(rank)
         break
-    if arr[i-1][1:] != arr[i][1:]:
-        rank += 1
+    elif arr[i-1][1:] != arr[i][1:]:
+        rank = i + 1
 else:
     print(rank)
