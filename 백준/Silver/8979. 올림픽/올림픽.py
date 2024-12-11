5cda23826367fe9c1086da3af2a0f7bc1528cd59
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-12-11 23:52:23 +0900

    [Silver V] Title: 올림픽, Time: 32 ms, Memory: 32412 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+arr.sort(key=lambda x: (x[1], x[2], x[3]))
+
+rank = 1
+
+for i in range(1, len(arr)):
+    if arr[i-1][0] == m:
+        print(rank)
+        break
+    if arr[i-1][1:] != arr[i][1:]:
+        rank += 1
+else:
+    print(rank)
