16bd4956cbdda7ca8e878796185f779f23f6cad1
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-12-16 10:07:51 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 136 ms, Memory: 44996 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,12 +1,14 @@
 import sys
 
-N = int(sys.stdin.readline())
-arr = []
-for i in range(N):
-    arr.append(list(map(int, sys.stdin.readline().split())))
+# input으로 들어오는 모든 값을 한 번에 읽기. n은 사실 필요 없으니 건너뛴다.
+xy = sys.stdin.readlines()[1:]
 
-arr.sort(key=lambda x:x[1])
-arr.sort(key=lambda x:x[0])
+# 값의 범위는 -100,000 ~ 100,000 이다.
+# 그러므로 x를 임계값인 200,001로 곱하고, y를 더하면
+# 각 행이 모두 독립적인 값을 가지게 되며
+# 정렬했을 때, 문제에서 요구하는 "x가 같으면 y를 기준으로 오름차순 정렬"에 맞게 정렬할 수 있다.
+xy.sort(key=lambda a: int(a.split()[0])*200_001+int(a.split()[1]))
 
-for i in arr:
-    print(*i)
+# 또한 strip()을 통해 '\n'을 지우지 않았으므로
+# 한 줄의 프린트로 자동으로 개행이 진행된다.
+print(''.join(xy))
