0bc265188eb4ba5e9ee6883db5538422b70d658e
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-12-21 19:46:23 +0900

    [Gold IV] Title: 빙산, Time: 508 ms, Memory: 157384 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,10 +1,15 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
 mat = [list(map(int, input().split())) for _ in range(n)]
 answer = -1
 cnt = 0
 while answer == -1:
     visit = [[0]*m for _ in range(n)]
-    q = []
+    q = deque()
     for y in range(n):
         if q:
             break
