a23683252545a690359146b438d655522f1141c2
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-12-23 16:30:56 +0900

    [Gold IV] Title: 빙산, Time: 2356 ms, Memory: 36080 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,42 +1,54 @@
 import sys
+from functools import lru_cache
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+@lru_cache(maxsize=None)
+def sol():
+    
+    from collections import deque
 
-mat = [list(map(int, input().split())) for _ in range(n)]
+    n, m = map(int, input().split())
 
-q = []
-cnt = 0
-answer = -1
-while answer == -1:
-    for y in range(n):
-        for x in range(m):
-            if mat[y][x]:
-                q.append((y, x))
+    mat = [list(map(int, input().split())) for _ in range(n)]
+
+    q = deque()
+    cnt = 0
+    answer = -1
+    while answer == -1:
+        for y in range(n):
+            for x in range(m):
+                if mat[y][x]:
+                    q.append((y, x))
+                    break
+            if q:
                 break
-        if q:
-            break
-    else:
-        answer = 0
-    visit = [[0] * m for _ in range(n)]
-    visit[y][x] = 1
-    while q:
-        y, x = q.pop()
-        for ny, nx in [(y+1, x), (y-1, x), (y, x+1), (y, x-1)]:
-            if 0 <= ny < n and 0 <= nx < m:
-                if mat[ny][nx] == 0:
-                    mat[y][x] -= 1
-                    if mat[y][x] == 0:
-                        mat[y][x] = -1
-                elif mat[ny][nx] > 0 and not visit[ny][nx]:
-                    visit[ny][nx] = 1
-                    q.append((ny, nx))
-    for y in range(n):
-        for x in range(m):
-            if mat[y][x] < 0:
-                mat[y][x] = 0
-            elif mat[y][x] > 0 and not visit[y][x]:
-                answer = cnt
-    cnt += 1
-print(answer)
+        else:
+            answer = 0
+        
+        visit = [[0] * m for _ in range(n)]
+        visit[y][x] = 1
+        while q:
+            y, x = q.pop()
+            
+            for ny, nx in [(y+1, x), (y-1, x), (y, x+1), (y, x-1)]:
+                if 0 <= ny < n and 0 <= nx < m:
+                    if mat[ny][nx] == 0:
+                        mat[y][x] -= 1
+                        if mat[y][x] == 0:
+                            mat[y][x] = -1
+                    elif mat[ny][nx] > 0 and not visit[ny][nx]:
+                        visit[ny][nx] = 1
+                        q.append((ny, nx))
+        
+        for y in range(n):
+            for x in range(m):
+                if mat[y][x] < 0:
+                    mat[y][x] = 0
+                elif mat[y][x] > 0 and not visit[y][x]:
+                    answer = cnt
+        
+        cnt += 1
+    print(answer)
+
+sol()
