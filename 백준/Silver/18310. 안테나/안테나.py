b317e8068e9a44ca09e21bda040295a518fae454
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-12-27 15:55:02 +0900

    [Silver III] Title: 안테나, Time: 100 ms, Memory: 53288 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.py
+++ 백준/Silver/18310. 안테나/안테나.py
@@ -1,26 +1,30 @@
-n = int(input())
-x = list(map(int, input().split()))
-arr = [0] * (max(x))
+def sol():
+    
+    n = int(input())
+    x = list(map(int, input().split()))
+    arr = [0] * max(x)
 
-for i in x:
-    arr[i-1] += 1
+    for i in x:
+        arr[i-1] += 1
 
-lc, rc = 0, sum(arr[1:])
-ls, rs = 0, sum([abs(0 - i) * arr[0] for i in range(len(arr))])
+    lc, rc = 0, sum(arr[1:])
+    ls, rs = 0, sum([abs(0 - i) * arr[0] for i in range(len(arr))])
 
-ms = ls+rs
-answer = 0
-for i in range(1, len(arr)):
-    if arr[i-1]:
-        lc += arr[i-1]
-    if arr[i]:
-        rc -= arr[i]
-    ls += lc
-    rs -= (arr[i] + rc)
-    if ms > ls+rs:
-        ms = ls+rs
-        answer = i
-    else:
-        break
+    ms = ls+rs
+    answer = 0
+    for i in range(1, len(arr)):
+        if arr[i-1]:
+            lc += arr[i-1]
+        if arr[i]:
+            rc -= arr[i]
+        ls += lc
+        rs -= (arr[i] + rc)
+        if ms > ls+rs:
+            ms = ls+rs
+            answer = i
+        elif ms < ls+rs:
+            break
 
-print(answer+1)
+    print(answer+1)
+
+sol()
