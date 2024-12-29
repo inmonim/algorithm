d6d739a05815fb648bb51cb45b5d692751389b1d
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-12-29 16:56:53 +0900

    [Gold V] Title: 단어 만들기, Time: 1236 ms, Memory: 54848 KB -BaekjoonHub

--- 백준/Gold/1148. 단어 만들기/단어 만들기.py
+++ 백준/Gold/1148. 단어 만들기/단어 만들기.py
@@ -2,33 +2,34 @@ import sys
 from collections import Counter
 
 input = sys.stdin.readline
+words = set()
+chars = []
+while (i := input().strip()) != "-":
+    words.add(i)
+while (i := input().strip()) != "#":
+    chars.append(i)
 
-def sol():
-    from collections import Counter
-    words = set()
-    chars = []
-    while (i := input().strip()) != "-":
-        words.add(i)
-    while (i := input().strip()) != "#":
-        chars.append(i)
-    for char in chars:
-        char_dict = Counter(char)
-        wd = {i: 0 for i in char}
-        for word in words:
-            word_dict = Counter(word)
-            for k, v in word_dict.items():
-                if char_dict.get(k) == None or char_dict[k] < v:
-                    break
-            else:
-                for k in word_dict.keys():
-                    wd[k] += 1
-        M, m = max(wd.values()), min(wd.values())
-        Mk, mk = [], []
-        for k, v in wd.items():
-            if v == M:
-                Mk.append(k)
-            if v == m:
-                mk.append(k)  
-        print(f"{''.join(sorted(mk))} {m} {''.join(sorted(Mk))} {M}")
+char_list = []
+for char in chars:
+    char_dict = Counter(char)
+    wd = {i: 0 for i in char}
+    char_list.append([char_dict, wd])
+for word in words:
+    word_dict = Counter(word)
+    for char_dict, wd in char_list:
+        for k, v in word_dict.items():
+            if k not in char_dict or char_dict[k] < v:
+                break
+        else:
+            for k in word_dict.keys():
+                wd[k] += 1
 
-sol()
+for char_dict, wd in char_list:
+    M, m = max(wd.values()), min(wd.values())
+    Mk, mk = [], []
+    for k, v in wd.items():
+        if v == M:
+            Mk.append(k)
+        if v == m:
+            mk.append(k)
+    print(f"{''.join(sorted(mk))} {m} {''.join(sorted(Mk))} {M}")
