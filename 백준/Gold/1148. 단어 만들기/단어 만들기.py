1bf33bc8ca619b15ba597aa1d7f1a880f76b6218
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-12-29 16:57:18 +0900

    [Gold V] Title: 단어 만들기, Time: 940 ms, Memory: 55036 KB -BaekjoonHub

--- 백준/Gold/1148. 단어 만들기/단어 만들기.py
+++ 백준/Gold/1148. 단어 만들기/단어 만들기.py
@@ -2,34 +2,38 @@ import sys
 from collections import Counter
 
 input = sys.stdin.readline
-words = set()
-chars = []
-while (i := input().strip()) != "-":
-    words.add(i)
-while (i := input().strip()) != "#":
-    chars.append(i)
 
-char_list = []
-for char in chars:
-    char_dict = Counter(char)
-    wd = {i: 0 for i in char}
-    char_list.append([char_dict, wd])
-for word in words:
-    word_dict = Counter(word)
+def sol():
+    words = set()
+    chars = []
+    while (i := input().strip()) != "-":
+        words.add(i)
+    while (i := input().strip()) != "#":
+        chars.append(i)
+
+    char_list = []
+    for char in chars:
+        char_dict = Counter(char)
+        wd = {i: 0 for i in char}
+        char_list.append([char_dict, wd])
+    for word in words:
+        word_dict = Counter(word)
+        for char_dict, wd in char_list:
+            for k, v in word_dict.items():
+                if k not in char_dict or char_dict[k] < v:
+                    break
+            else:
+                for k in word_dict.keys():
+                    wd[k] += 1
+
     for char_dict, wd in char_list:
-        for k, v in word_dict.items():
-            if k not in char_dict or char_dict[k] < v:
-                break
-        else:
-            for k in word_dict.keys():
-                wd[k] += 1
+        M, m = max(wd.values()), min(wd.values())
+        Mk, mk = [], []
+        for k, v in wd.items():
+            if v == M:
+                Mk.append(k)
+            if v == m:
+                mk.append(k)
+        print(f"{''.join(sorted(mk))} {m} {''.join(sorted(Mk))} {M}")
 
-for char_dict, wd in char_list:
-    M, m = max(wd.values()), min(wd.values())
-    Mk, mk = [], []
-    for k, v in wd.items():
-        if v == M:
-            Mk.append(k)
-        if v == m:
-            mk.append(k)
-    print(f"{''.join(sorted(mk))} {m} {''.join(sorted(Mk))} {M}")
+sol()
