bfbafef6bf657cd05282ea41620e44e1627066c3
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2024-12-29 16:31:23 +0900

    [Gold V] Title: 단어 만들기, Time: 3536 ms, Memory: 54840 KB -BaekjoonHub

--- 백준/Gold/1148. 단어 만들기/단어 만들기.py
+++ 백준/Gold/1148. 단어 만들기/단어 만들기.py
@@ -2,32 +2,33 @@ import sys
 from collections import Counter
 
 input = sys.stdin.readline
-words = set()
-chars = []
-while (i := input().strip()) != "-":
-    words.add(i)
-while (i := input().strip()) != "#":
-    chars.append(i)
 
-for char in chars:
-    char_dict = Counter(char)
-    wd = {i: 0 for i in char}
-    for word in words:
-        word_dict = Counter(word)
-        for k, v in word_dict.items():
-            
-            if char_dict.get(k) == None or char_dict[k] < v:
-                break
-        else:
+def sol():
+    from collections import Counter
+    words = set()
+    chars = []
+    while (i := input().strip()) != "-":
+        words.add(i)
+    while (i := input().strip()) != "#":
+        chars.append(i)
+    for char in chars:
+        char_dict = Counter(char)
+        wd = {i: 0 for i in char}
+        for word in words:
+            word_dict = Counter(word)
             for k, v in word_dict.items():
-                wd[k] = 1 if wd.get(k) == None else wd[k] + 1
-    
-    M, m = max(wd.values()), min(wd.values())
-    Mk, mk = [], []
-    for k, v in wd.items():
-        if v == M:
-            Mk.append(k)
-        if v == m:
-            mk.append(k)
-            
-    print(f"{''.join(sorted(mk))} {m} {''.join(sorted(Mk))} {M}")
+                if char_dict.get(k) == None or char_dict[k] < v:
+                    break
+            else:
+                for k in word_dict.keys():
+                    wd[k] += 1
+        M, m = max(wd.values()), min(wd.values())
+        Mk, mk = [], []
+        for k, v in wd.items():
+            if v == M:
+                Mk.append(k)
+            if v == m:
+                mk.append(k)  
+        print(f"{''.join(sorted(mk))} {m} {''.join(sorted(Mk))} {M}")
+
+sol()
