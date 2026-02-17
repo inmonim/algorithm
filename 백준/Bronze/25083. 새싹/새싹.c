de4c3b71c69ebc34037e2f5783c3c347154843b2
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2026-02-17 17:00:44 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  char sprout[100] = "         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |";
+  printf("%s", sprout);
+  return 0;
+}
