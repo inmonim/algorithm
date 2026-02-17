bd50597922072eece192b620870500330bad9bb4
jeong-inmo <99791739+inmonim@users.noreply.github.com>
2026-02-17 18:07:43 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 204 ms, Memory: 12836 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    long long n;
-    scanf("%lld", &n);
+    int n;
+    if (scanf("%d", &n) != 1) {
+        return 0;
+    }
+
+    size_t cap = (size_t)n * 12 + 1;
+    char *out = (char *)malloc(cap);
+    if (!out) {
+        return 0;
+    }
+    size_t pos = 0;
     
-    for (long long i=0; i<n; i++) {
+    for (int i=0; i<n; i++) {
         int a, b;
         scanf("%d %d", &a, &b);
-        printf("%d\n", a+b);
+        pos += (size_t)snprintf(out + pos, cap - pos, "%d\n", a+b);
     }
+
+    fwrite(out, 1, pos, stdout);
+    free(out);
+    return 0;
 }
